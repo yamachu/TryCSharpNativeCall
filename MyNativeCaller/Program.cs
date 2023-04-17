dad668686cb850c7e4209673468c615d47244d3d
yamachu <yamachu.dev@gmail.com>
2023-04-17 23:35:41 +0900

    Initial commit

---
+++ MyNativeCaller/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+var nativeLib = args[0];
+
+var lib = NativeLibrary.Load(nativeLib);
+var handle = NativeLibrary.GetExport(lib, "my_cs_func");
+var fn = Marshal.GetDelegateForFunctionPointer<Action>(handle);
+
+Console.WriteLine(lib);
+
+fn();
+fn();
+fn();
+
+NativeLibrary.Free(lib);
---
+++ MyNativeLibrary/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MyNativeLibrary;
+
+public class Class1
+{
+    private static int Num = 0;
+
+    [UnmanagedCallersOnly(EntryPoint = "my_cs_func")]
+    public static void MyCSFunc()
+    {
+        Console.WriteLine($"Hello from C#! C# から こんにちは!, Num: {Num}");
+        Num++;
+    }
+}
